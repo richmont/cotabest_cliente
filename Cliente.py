c25cec9fcec2a3c03d45a9e7fbc1944607d8165d
Richelmy <richelmy.monteiro@gmail.com>
2023-04-29 13:40:11 -0300

    Cliente funcionou, constatou que não há lojas que não possuam horário de domingo

---
+++ Cliente.py
@@ -0,0 +1,60 @@
+import requests
+import urllib3
+urllib3.disable_warnings()
+import json
+
+class Loja():
+    def __init__(self, json_dados) -> None:
+        self.id_loja = json_dados["id"]
+        self.nome = json_dados["name"]
+        self.slug = json_dados["slug"]
+        self.abertura_especial = json_dados["special_opening"]
+        if len(self.abertura_especial) != 0:
+            raise IndexError(f"Loja {self.nome} possui abertura especial")
+
+        if len(json_dados["opening_hours"]) > 1:
+            self.abre_domingo = True
+            for dia in json_dados["opening_hours"]:
+                if dia["type"] == "domingo":
+                    self.horario_abertura_domingo = dia["opening"]
+                    self.horario_fechamento_domingo = dia["closure"]
+                if dia["type"] == "segunda-a-sabado":
+                    self.horario_abertura_semana = dia["opening"]
+                    self.horario_fechamento_semana = dia["closure"]
+            
+        else:
+            self.abre_domingo = False
+            if dia["type"] == "segunda-a-sabado":
+                    self.horario_abertura_semana = dia["opening"]
+                    self.horario_fechamento_semana = dia["closure"]
+            else:
+                raise IndexError(f"Loja {self.nome} abre em dia diferente de segunda a sábado")
+        
+        
+        
+
+class Cliente():
+    def __init__(self, latitude:float = -23.56571961652763, longitude:float = -46.651259360931014) -> None:
+        self.lista_lojas = []
+        url_api = f"https://apis.cotabest.com.br/folhetos/stores?latitude={latitude}&longitude={longitude}"
+        with requests.get(url_api, verify=False) as r:
+            self.json_bruto = r.json()
+            if r.status_code == 200:
+                if r.json()["success"]:
+                    self.total_lojas = int(r.json()["total"])
+                    self.conteudo_lojas_bruto = r.json()["data"]
+        
+        #print(self.conteudo_lojas_bruto[0])
+        
+        for loja_bruto in self.conteudo_lojas_bruto:
+            loja = Loja(loja_bruto["store"])
+            self.lista_lojas.append(loja)
+
+if __name__ == "__main__":
+    c = Cliente()
+    
+    for loja in c.lista_lojas:
+        print(loja.nome)
+        if loja.abre_domingo is not True:
+            print(f"Esta loja não abre domingo: {loja.nome}")
+    print("Lojas obtidas no programa: ", len(c.lista_lojas))
